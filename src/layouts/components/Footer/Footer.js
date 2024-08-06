ba98f606f44f08464794e7908c2a0e86633c434f
PhuocNguyen <bt.nhphuoc@gmail.com>
2024-08-06 11:48:35 +0700

    Giao diện Footer phần 1

--- src/layouts/components/Footer/Footer.js
+++ src/layouts/components/Footer/Footer.js
@@ -1,7 +1,18 @@
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import { Container } from 'react-bootstrap';
+
+const cx = classNames.bind(styles);
+
 function Footer() {
   return (
-    <footer className="wrapper">
-      <h1>Footer</h1>
+    <footer className={cx('wrapper')}>
+      <Container className="h-100">
+        <div className={cx('inner')}>
+          <p className={cx('copy-right')}>Copyright © 2019 PIXINVENT</p>
+          <p className={cx('desc')}>Hand-crafted & Made with</p>
+        </div>
+      </Container>
     </footer>
   );
 }
